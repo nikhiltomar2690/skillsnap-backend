d91b8bac4e65d280ac7f6b585f6af0c29ad3b381
nikhiltomar2690 <nikhiltomar2690@gmail.com>
2024-08-28 12:22:55 +0530

    zeptomail email service completed

--- dist/utils/sendMail.js
+++ dist/utils/sendMail.js
@@ -1,24 +1,53 @@
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+// dotenv.config();
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+// async function sendEmail(email: string, verificationCode: string) {
+//   console.log("Sending verification email to:", email);
+//   const mailOptions = {
+//     from: process.env.EMAIL_USER,
+//     to: email,
+//     subject: "Verify your SkillSnap Account",
+//     text: `Your verification code is: ${verificationCode}`, // Corrected syntax
+//   };
+//   try {
+//     const Transporter = await transporter.sendMail(mailOptions);
+//     console.log("Verification email sent:", Transporter.response);
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     throw new Error("Email could not be sent");
+//   }
+// }
+// export default sendEmail;
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.zeptomail.in",
+    port: 587, // TLS port
+    secure: false,
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.ZEPTOMAIL_USER,
+        pass: process.env.ZEPTOMAIL_API_KEY,
     },
 });
 async function sendEmail(email, verificationCode) {
     console.log("Sending verification email to:", email);
     const mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: process.env.ZEPTOMAIL_SENDER,
         to: email,
         subject: "Verify your SkillSnap Account",
-        text: `Your verification code is: ${verificationCode}`, // Corrected syntax
+        text: `Your verification code is: ${verificationCode}`,
     };
     try {
-        const Transporter = await transporter.sendMail(mailOptions);
-        console.log("Verification email sent:", Transporter.response);
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Verification email sent:", info.response);
     }
     catch (error) {
         console.error("Error sending email:", error);
