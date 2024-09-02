8d67745e5e73ad48ac11c8539a5b2ef7f4d4d673
nikhiltomar2690 <nikhiltomar2690@gmail.com>
2024-09-02 18:55:15 +0530

    skills and language schema merged into User schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -20,8 +20,50 @@ const userSchema = new mongoose.Schema({
   // References to other collections
   education: [{ type: mongoose.Schema.Types.ObjectId, ref: "Education" }],
   experience: [{ type: mongoose.Schema.Types.ObjectId, ref: "Experience" }],
-  skills: [{ type: mongoose.Schema.Types.ObjectId, ref: "Skill" }],
-  languages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Language" }],
+  skills: [
+    {
+      skillName: {
+        type: String,
+        required: [true, "Skill name is required"],
+        minlength: [2, "Skill name must be at least 2 characters long"],
+      },
+      level: {
+        type: String,
+        enum: {
+          values: ["Beginner", "Intermediate", "Advanced", "Expert"],
+          message:
+            "Level must be either Beginner, Intermediate, Advanced, or Expert",
+        },
+      },
+      link: {
+        type: String,
+        validate: {
+          validator: function (v: string) {
+            // regex to validate email
+            return v === "" || /^https?:\/\/.+/i.test(v);
+          },
+          message: "Link must be a valid URL",
+        },
+      },
+    },
+  ],
+  languages: [
+    {
+      languageName: {
+        type: String,
+        required: [true, "Language name is required"],
+        minlength: [2, "Language name must be at least 2 characters long"],
+      },
+      level: {
+        type: String,
+        enum: {
+          values: ["Beginner", "Intermediate", "Advanced", "Fluent", "Native"],
+          message:
+            "Level must be either Beginner, Intermediate, Advanced, Fluent, or Native",
+        },
+      },
+    },
+  ],
   certificates: [{ type: mongoose.Schema.Types.ObjectId, ref: "Certificate" }],
   interests: [{ type: mongoose.Schema.Types.ObjectId, ref: "Interest" }],
   courses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
